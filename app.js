d49723109f0c69bc948cb1f72691f0b562221107
HM112183 <harshmalaviya1121@gmail.com>
2026-02-05 22:25:44 +0530

    locally running

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
-
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-const port = process.env.PORT || 3000;
+const express = require('express');
+const path = require('path');
 const app = express();
+const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname+'/public')))
-
-// Fix __dirname in ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 // Serve static files from current folder
 app.use(express.static(__dirname));
 
